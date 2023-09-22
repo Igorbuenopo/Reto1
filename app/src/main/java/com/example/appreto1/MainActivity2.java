f9fe23024e736dbbb31ceb774f3178faf0142b4b
Igorbuenopo <igor.buenopo@elorrieta-errekamari.com>
2023-09-22 14:14:12 +0200

    push menuu 2

--- app/src/main/java/com/example/appreto1/MainActivity2.java
+++ app/src/main/java/com/example/appreto1/MainActivity2.java
@@ -1,8 +1,11 @@
 package com.example.appreto1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.imageView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
