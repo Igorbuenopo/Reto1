0c30039dbee23ddec78ef7bc775020f9297f2595
unaxG <122366560+unaxG@users.noreply.github.com>
2023-09-22 12:36:53 +0200

    pushActivitiesEmpty unax

---
+++ app/src/main/java/com/example/appreto1/ActivityCalcNote.java
@@ -0,0 +1,20 @@
+package com.example.appreto1;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityCalcNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_CalcNote);
+
+    }
+}
---
+++ app/src/main/java/com/example/appreto1/ActivityEvent.java
@@ -0,0 +1,15 @@
+package com.example.appreto1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Event);
+
+    }
+}
